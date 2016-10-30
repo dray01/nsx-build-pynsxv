8f68e02a6626fb4b79c266f7b549f9c27b784ebf
dray01 <brett.drayton@gmail.com>
2016-10-30 19:04:20 +1100

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Edit with names as required.
+
+pynsxv dlr delete -n py-dlr01
+pynsxv esg delete -n py-esg01
+pynsxv lswitch delete -n py-web01
+pynsxv lswitch delete -n py-app01
+pynsxv lswitch delete -n py-db01
+pynsxv lswitch delete -n py-transport01
